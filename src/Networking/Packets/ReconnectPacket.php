5ce0347d92455783346b618776918166b369129e
Misaka9876 <1653573879@qq.com>
2021-11-30 12:53:04 +0800

    新增俩个Packet

---
+++ src/Networking/Packets/ReconnectPacket.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Botuild\GuildBotProtocol\Networking\Packets;
+
+use Botuild\GuildBotProtocol\Networking\BasePacket;
+use Botuild\GuildBotProtocol\Networking\Client\ApiClient;
+use Botuild\GuildBotProtocol\Networking\Packet;
+
+class ReconnectPacket implements Packet
+{
+    public static function getPacketInformation(): array
+    {
+        return [
+            'opcode' => 7,
+            'name' => 'Reconnect'
+        ];
+    }
+
+    public static function parse(ApiClient $client, BasePacket $packet)
+    {
+        return new ReconnectPacket();
+    }
+
+    public function pack(): BasePacket
+    {
+        return new BasePacket(7);
+    }
+}
